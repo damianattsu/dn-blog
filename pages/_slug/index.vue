945c99983a850dbd123b7e7a3052dc4b53405a6d
damianattsu <damianattsu@gmail.com>
2021-02-07 23:24:38 +0900

    scss

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -25,18 +25,47 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
 .wrapper {
   max-width: 1200px;
   margin: 0 auto;
   padding: 0 15px;
 }
-.post p {
-  font-size: 24px;
+.title {
+  margin-bottom: 20px;
 }
-.post img {
-  display: block;
-  max-width: 100%;
-  height: auto;
+
+.publishedAt {
+  margin-bottom: 40px;
+}
+
+.post {
+  & > h1 {
+    font-size: 30px;
+    font-weight: bold;
+    margin: 40px 0 20px;
+    background-color: #eee;
+    padding: 10px 20px;
+    border-radius: 5px;
+  }
+  & > h2 {
+    font-size: 24px;
+    font-weight: bold;
+    margin: 40px 0 16px;
+    border-bottom: 1px solid #ddd;
+  }
+  & > p {
+    line-height: 1.8;
+    letter-spacing: 0.2px;
+    img {
+      max-width: 100%;
+      display: block;
+      height: auto;
+    }
+  }
+  & > ol {
+    list-style-type: decimal;
+    list-style-position: inside;
+  }
 }
 </style>
