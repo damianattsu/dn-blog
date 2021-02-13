04b99d9dbf7e97f07f90050b4ca0beb1b6c59171
damianattsu <damianattsu@gmail.com>
2021-02-14 00:37:11 +0900

    ラベル調整

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,13 +1,17 @@
 <template>
   <section class="contents">
     <div class="wrapper">
-      <h1 class="title">{{ title }}</h1>
-      <p class="publishedAt">{{ publishedDate() }}</p>
-      <p class="category">{{ category && category.name }}</p>
-      <div v-if="main_image" class="image-wrapper">
-        <img :src="main_image.url" alt="">
+      <div class="post-wrapper">
+        <h1 class="title">{{ title }}</h1>
+        <div class="post-label">
+          <p class="category">{{ category && category.name }}</p>
+          <p class="date-published">{{ publishedDate() }}</p>
+        </div>
+        <div v-if="main_image" class="image-wrapper">
+          <img :src="main_image.url" alt="">
+        </div>
+        <div class="post" v-html="body"></div>
       </div>
-      <div class="post" v-html="body"></div>
       <div class="home-back">
         <nuxt-link to="/">
           戻る
@@ -44,48 +48,63 @@ export default {
 </script>
 
 <style lang="scss">
-.title {
-  font-size: 20px;
-  font-weight: bold;
-  letter-spacing: 0.5px;
-  margin-bottom: 15px;
-}
-.publishedAt {
-  font-size: 14px;
-  margin-bottom: 10px;
-}
-.image-wrapper {
-  margin-bottom: 20px;
-  img {
-    max-width: 100%;
-    display: block;
-    height: auto;
-  }
-}
-.post {
-  & > h1 {
-    font-size: 24px;
-    font-weight: bold;
-    letter-spacing: 0.5px;
-    margin: 40px 0 20px;
-  }
-  & > h2 {
+.post-wrapper {
+  .title {
     font-size: 20px;
     font-weight: bold;
-    margin: 40px 0 16px;
-    border-bottom: 1px solid #ddd;
+    letter-spacing: 0.5px;
+    margin-bottom: 15px;
   }
-  & > img {
-    max-width: 100%;
-    display: block;
-    height: auto;
-  }
-  & > pre {
-    background-color: #333;
-    padding: 15px 15px;
-    margin-top: 20px;
-    code {
+  .post-label {
+    margin-bottom: 10px;
+    .category {
       color: #fff;
+      font-size: 12px;
+      font-weight: bold;
+      line-height: 1;
+      background-color: #333;
+      padding: 8px 10px;
+      display: inline-block;
+      margin-right: 5px;
+    }
+    .date-published {
+      font-size: 14px;
+      display: inline-block;
+    }
+  }
+  .image-wrapper {
+    margin-bottom: 20px;
+    img {
+      max-width: 100%;
+      display: block;
+      height: auto;
+    }
+  }
+  .post {
+    & > h1 {
+      font-size: 24px;
+      font-weight: bold;
+      letter-spacing: 0.5px;
+      margin: 40px 0 20px;
+    }
+    & > h2 {
+      font-size: 20px;
+      font-weight: bold;
+      margin: 40px 0 16px;
+      border-bottom: 1px solid #ddd;
+    }
+    & > img {
+      max-width: 100%;
+      display: block;
+      height: auto;
+    }
+    & > pre {
+      background-color: #333;
+      padding: 15px 15px;
+      margin-top: 20px;
+      code {
+        color: #fff;
+      }
     }
   }
 }
